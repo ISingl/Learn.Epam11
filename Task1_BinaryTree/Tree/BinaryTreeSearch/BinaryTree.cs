c71030e53cb9307ad95ebb5654b1eaa90bf65831
Nogin Sergey <ver-s_online@mail.ru>
2020-09-25 01:00:25 +0400

    Add BinaryTree Class, test class or console

--- Task1_BinaryTree/Tree/BinaryTreeSearch/BinaryTree.cs
+++ Task1_BinaryTree/Tree/BinaryTreeSearch/BinaryTree.cs
@@ -6,19 +6,74 @@ using System.Threading.Tasks;
 
 namespace BinaryTreeSearch
 {
-    public class BinaryTree
+    public class Node<T>
+        where T : IComparable
     {
-        public class Node<T>
-        {
-            public T Data { get; private set; }
-            public Node<T> Left{ get; private set; }
-            public Node<T> Right { get; private set; }
+        public T Data { get; private set; }
+        public Node<T> Left { get; private set; }
+        public Node<T> Right { get; private set; }
 
-            public Node(T data)
+        public Node(T data)
+        {
+            Data = data;
+        }
+
+        public void Add(T data)
+        {
+            var node = new Node<T>(data);
+
+            if (node.CompareTo(Data) == -1)
             {
-                Data = data;
+                if(Left == null)
+                {
+                    Left = node;
+                }
+                else
+                {
+                    Left.Add(data);
+                }
+            }
+            else
+            {
+                if(Right == null)
+                {
+                    Right = node;
+                }
+                else
+                {
+                    Right.Add(data);
+                }
+            }
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj is T item)
+            {
+                return Data.CompareTo(item);
+            }
+            else
+                throw new ArgumentException();
+        }
+    }
+
+    public class BinaryTree<T>
+        where T : IComparable
+    {       
+        public Node<T> Root { get; private set; }
+        public int Count { get; private set; }
+
+        public void Add(T data)
+        {
+            if(Root == null)
+            {
+                Root = new Node<T>(data);
+                Count = 1;
+                return;
             }
 
+            Root.Add(data);
+            Count++;
         }
     }
 }
---
+++ Task1_BinaryTree/Tree/Tree_UI/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BinaryTreeSearch;
+
+namespace Tree_UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree<int> tree = new BinaryTree<int>();
+            tree.Add(5);
+            tree.Add(3);
+            tree.Add(7);
+            tree.Add(1);
+            tree.Add(2);
+            tree.Add(8);
+            tree.Add(6);
+            tree.Add(9);
+
+        }
+    }
+}
